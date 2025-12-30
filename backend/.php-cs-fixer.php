b6c495fdd1e9895e3dde32302012a56d5580e7fa
Pavel Danilov <dpv@aihub.ws>
2025-12-30 13:41:02 +0300

    zzeraw/invoicio.php#2 поддержка енумов

---
+++ backend/.php-cs-fixer.php
@@ -0,0 +1,22 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src/AppBundle',
+        __DIR__ . '/src/InvoiceBundle',
+        __DIR__ . '/src/UserBundle',
+    ])
+    ->name('*.php')
+    ->exclude(['Tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'yoda_style' => [
+            'equal' => true,
+            'identical' => true,
+            'less_and_greater' => false,
+        ],
+    ])
+    ->setFinder($finder);
